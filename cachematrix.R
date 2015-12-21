c27b5f38c99086f688a2e6e099fe96cb06cb537c
geofhileman <geof.hileman@gmail.com>
2015-12-21 15:57:51 -0500

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## These functions determine the inverse of a matrix if needed and if not pull the result from memory
 
-
+# makeCacheMatrix establishes the set and get functions for determining the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -17,7 +17,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-
+# cacheSolve determines whether a new calculation is necessary
+# if necessary, the calculation is performed
+# if available in memory, the solution is retrieved
 cacheSolve <- function(x, ...) {
         m <- x$getinv()
         if(!is.null(m)) {
